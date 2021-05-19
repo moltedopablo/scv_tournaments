8ee3b6a1e9799536f85cc998751841f98fecd4e6
Pablo Moltedo <moltedopablo@gmail.com>
2021-05-19 20:46:55 -0300

    Added scss for bootstrap theming

--- frontend/scv_tournaments/src/plugins/bootstrap-vue.js
+++ frontend/scv_tournaments/src/plugins/bootstrap-vue.js
@@ -4,5 +4,7 @@ import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import './app.scss'
+
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
