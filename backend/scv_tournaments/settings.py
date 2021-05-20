d0b2d2b28ce7abcc141e914496d766e2643b5910
Pablo Moltedo <moltedopablo@gmail.com>
2021-05-20 15:56:03 -0300

    Added docker support

--- backend/scv_tournaments/settings.py
+++ backend/scv_tournaments/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s!=^01ns&ztu9^o6!soe=5us#&a6xkja)3_)u=wx#&o((bvcul
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django-api']
 
 
 # Application definition
--- frontend/scv_tournaments/vue.config.js
+++ frontend/scv_tournaments/vue.config.js
@@ -1,15 +1,23 @@
 /**
- * @type {import('@vue/cli-service').ProjectOptions}
+ * @type {string}
  */
+let apiHost;
+//Use enviroment variable for docker setup, localhost for local dev
+if (process.env.API_HOST) {
+    apiHost = process.env.API_HOST
+} else {
+    apiHost = 'localhost'
+}
+
 module.exports = {
-  devServer: {
-    proxy: {
-      "^/backend": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        logLevel: "debug",
-        pathRewrite: { "^/backend": "/" }
-      }
+    devServer: {
+        proxy: {
+            "^/backend": {
+                target: 'http://' + apiHost + ':8000',
+                changeOrigin: true,
+                logLevel: "debug",
+                pathRewrite: {"^/backend": "/"}
+            }
+        }
     }
-  }
 };
