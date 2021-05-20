9baf5e1b3cb6fca71075791d2fdc519dd1cab1bf
Pablo Moltedo <moltedopablo@gmail.com>
2021-05-20 11:56:22 -0300

    Change card layout for tournaments

--- frontend/scv_tournaments/src/components/Tournaments.vue
+++ frontend/scv_tournaments/src/components/Tournaments.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <b-card header-tag="header">
+    <b-card no-body header-tag="header">
       <template #header>
         <h6 class="mb-0">Seleccionar Torneo</h6>
       </template>
-      <b-card-text>
-        <b-alert :show="!tournaments.length" variant="secondary">No hay torneos cargados</b-alert>
-        <b-list-group>
-          <b-list-group-item v-for="t in tournaments" :key="t.id" v-on:click="selectTournament(t.id)" button>
-            {{ t.name }}
-          </b-list-group-item>
-        </b-list-group>
-      </b-card-text>
+      <b-list-group flush>
+        <b-list-group-item v-for="t in tournaments" :key="t.id" v-on:click="selectTournament(t.id)" button>
+          {{ t.name }}
+        </b-list-group-item>
+      </b-list-group>
+      <b-card-body>
+        <span class="mt-3 p-3" v-if="!tournaments.length">No hay torneos cargados</span>
+      </b-card-body>
     </b-card>
   </div>
 </template>
